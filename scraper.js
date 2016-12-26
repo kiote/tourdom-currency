a3eefe17ee45a685b3fd35af109c227ce81bae36
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2016-12-26 19:45:52 +0300

    change format

--- scraper.js
+++ scraper.js
@@ -15,22 +15,25 @@ Rate.remove({}, function(err, removed){
 // parse
 var datasource = 'https://openexchangerates.org/api/latest.json?app_id=' + process.env.OPENEXCHANGERATE
 request(datasource, function (err, result, body) {
+  if (err) {
+    console.log(err);
+  }
   if (!err && result.statusCode == 200) {
     var rates = JSON.parse(body);
     var eur = rates['rates']['EUR'];
     var rub = rates['rates']['RUB'];
-    new Rate({'base': 'usd',
-              'usd': 1,
-              'eur': eur,
-              'rub': rub}).save();
-    new Rate({'base': 'rub',
-              'rub': 1,
-              'usd': 1/rub,
-              'eur': eur/rub}).save();
-    new Rate({'base': 'eur',
-              'eur': 1,
-              'usd': 1/eur,
-              'rub': rub/eur}).save();
+    new Rate({'usd': {
+                'usd': 1,
+                'eur': eur,
+                'rub': rub }}).save();
+    new Rate({'rub': {
+                'rub': 1,
+                'usd': 1/rub,
+                'eur': eur/rub }}).save();
+    new Rate({'eur' : {
+                'eur': 1,
+                'usd': 1/eur,
+                'rub': rub/eur}}).save();
     console.log(rub);
   }
 })
